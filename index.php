c21770e8a1628966ac2ef02950ce0584258a59bc
Hemant Mann <hemant.mann121@gmail.com>
2016-07-03 17:34:30 +0530

    Server config

--- index.php
+++ index.php
@@ -10,6 +10,22 @@ define("CDN", "/public/assets/");
 date_default_timezone_set('Asia/Kolkata');
 
 try {
+	
+	if (!function_exists('getallheaders')) 
+	{ 
+	    function getallheaders() 
+	    { 
+	           $headers = ''; 
+	       foreach ($_SERVER as $name => $value) 
+	       { 
+	           if (substr($name, 0, 5) == 'HTTP_') 
+	           { 
+	               $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
+	           } 
+	       } 
+	       return $headers; 
+	    } 
+	} 
     
     // library's class autoloader
     spl_autoload_register(function($class) {
